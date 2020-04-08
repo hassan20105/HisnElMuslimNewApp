a76a6cb9a5c8bdc0d1a8cf95314d2c68cb3d895a
hassan20105 <hassanhassanragheb@outlook.com>
2020-04-09 00:49:42 +0200

    add couter Feature

--- app/src/main/java/com/hisnElMuslem/myapplication/Fragments/DynamicFragment.java
+++ app/src/main/java/com/hisnElMuslem/myapplication/Fragments/DynamicFragment.java
@@ -1,22 +1,28 @@
 package com.hisnElMuslem.myapplication.Fragments;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
 
 import com.hisnElMuslem.myapplication.R;
 
-public class DynamicFragment extends Fragment {
+public class DynamicFragment extends Fragment implements View.OnClickListener {
     private TextView display_TV,countTV,refTV ,descTV;
-    private ImageView img_div;
-
+    private ImageView resetImgView;
+    private CardView cardview;
+   private int countValue;
+   private String msg,count,ref,desc;
 
     @Nullable
     @Override
@@ -26,26 +32,41 @@ public class DynamicFragment extends Fragment {
         countTV = view.findViewById(R.id.countTV);
         refTV = view.findViewById(R.id.refTV);
         descTV = view.findViewById(R.id.desc_TV);
-        img_div = view.findViewById(R.id.img_div);
-        String msg = getArguments().getString("msg");
-        String count = getArguments().getString("count");
-        String ref = getArguments().getString("ref");
-        String desc = getArguments().getString("desc");
-        if(!desc.equals(null)) {
-            descTV.setVisibility(View.VISIBLE);
-            img_div.setVisibility(View.VISIBLE);
-            descTV.setText(desc);
-        }
-        else {
-            descTV.setVisibility(View.INVISIBLE);
-            img_div.setVisibility(View.INVISIBLE);
-        }
+        cardview = view.findViewById(R.id.cardview);
+        resetImgView = view.findViewById(R.id.resetImgView);
+         msg = getArguments().getString("msg");
+         count = getArguments().getString("count");
+         ref = getArguments().getString("ref");
+         desc = getArguments().getString("desc");
+        countTV.setText(count);
+        countValue = Integer.parseInt(countTV.getText().toString());
+        countTV.setOnClickListener(this);
+        resetImgView.setOnClickListener(this);
         refTV.setText(ref);
         display_TV.setText(msg);
-        if(count.equals(null)||count.equals("1")||count.equals("مرة وأحدة"))
-        countTV.setText("مرة واحدة");
-        else
-            countTV.setText(count);
         return view;
+    }
+
+
+    @SuppressLint("ResourceAsColor")
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.countTV:
+                countValue = Integer.parseInt(countTV.getText().toString());
+                if(countValue<1) {
+                    cardview.setCardBackgroundColor(R.color.pink);
+                }
+                else {
+                    countValue -= 1;
+                    countTV.setText("" + countValue);
+                }
+                break;
+            case R.id.resetImgView:
+                countTV.setText(count);
+                cardview.setCardBackgroundColor(R.color.pink);
+                break;
+
+        }
     }
 }
